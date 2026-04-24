54dad98e657e3bb276bd8d43fc4e6fa537ec9a29
prudhvimallepudi <prudhvinarasimha28@gmail.com>
2026-04-24 18:14:05 +0530

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE = process.env.EXPO_PUBLIC_BACKEND_URL;
+const BASE = "https://mall-nroh.onrender.com";
 
 export const api = axios.create({
   baseURL: `${BASE}/api`,
